47ce39b8f3e2c40e67061e1d4860b5ee3c9ec757
nguyenduytuong04 <tuongnguyenduytuong076@gmail.com>
2025-02-24 10:34:02 +0700

    init project

---
+++ src/main/java/com/example/NguyenDuyTuong/NguyenDuyTuongApplication.java
@@ -0,0 +1,13 @@
+package com.example.NguyenDuyTuong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NguyenDuyTuongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NguyenDuyTuongApplication.class, args);
+	}
+
+}
